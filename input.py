db607cc7f1f489f780f5ffc0666caa6b51178c3b
yaseen <mohammedbyasin@gmail.com>
2020-12-22 11:05:49 +0530

    Day -1

---
+++ input.py
@@ -0,0 +1,3 @@
+my_name = input("Enter your name : ")
+print("Hello " + my_name)
+print("Length of the name: " + str(len(my_name)))
---
+++ print_quotation.py
@@ -0,0 +1,3 @@
+print("Hello " + "Yasin")
+print("Hello 'Mr.' " + "Yasin")  # We cannot same quotes if you want explicitly print a word with quotes
+print('Hello "Mr." ' + "Yasin")
