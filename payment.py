c142c82237a7d485e3b60d32cda6caeef02c43f8
yaseen <mohammedbyasin@gmail.com>
2020-12-23 21:54:47 +0530

    reformat

--- payment.py
+++ payment.py
@@ -9,7 +9,7 @@ tips_percent = int(input("Enter % of tips to be give 12 or 15 or 17 : "))
 share_person = int(input("Enter the number of persons sharing : "))
 # print (share_person)
 
-bill_with_tip = (bill_amount * (tips_percent/100) + bill_amount) / share_person
+bill_with_tip = (bill_amount * (tips_percent / 100) + bill_amount) / share_person
 pay_per_person = round(bill_with_tip, 2)
 print(f"\nPayment per person is {pay_per_person}")
 pay_per_person = "{:.2f}".format(bill_with_tip)  # This format rounds to 2 decimal through actual result doesn't has
